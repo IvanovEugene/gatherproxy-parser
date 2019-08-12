7c3ddfe8e7d2858a8b56e14588865d660c39b795
Ivanov Eugene <erqups@gmail.com>
2019-08-12 13:15:15 +0300

    feat: add exception classes for ProxyParser

---
+++ gatherparser/errors/proxy_parser.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+class ProxyParserException(Exception):
+    """Abstract ProxyParser exception"""
+    pass
+
+
+class ProxyParserFetchError(ProxyParserException):
+    """Raised when Selenium unable to open page"""
+    pass
+
+
+class ProxyParserDOMError(ProxyParserException):
+    """Raised when Selenium unable to find required element in Document Object Model"""
+    pass
