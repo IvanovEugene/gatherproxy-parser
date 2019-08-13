fe0742170bca38cf4973fd13979ff13893b0f040
Ivanov Eugene <erqups@gmail.com>
2019-08-13 11:17:41 +0300

    feat: update example

--- examples/chromedriver_mac.py
+++ examples/chromedriver_mac.py
@@ -1,22 +1,21 @@
-# encoding: utf-8 #
+# encoding: utf-8
 import os
+import asyncio
 from gatherparser import ProxyCollector
 
 
-if __name__ == "__main__":
+async def main():
     URL_TO_PARSE = "http://www.gatherproxy.com/proxylist/country/?c=Russia"
-    PAGE_COUNT = 5
+    PAGE_COUNT = 10
     DRIVER_TYPE = "chrome"
 
     driver_kwargs = {
-        "driver_path": os.path.join("tools", "drivers", "chrome",
-                                    "macOS", "chromedriver"),
+        "driver_path": os.path.join("tools", "drivers", "chrome", "macOS", "chromedriver"),
         "page_load_timeout": 60
     }
     validator_kwargs = {
         "proxy_verification_link": "https://google.com",
-        "proxy_timeout": 30,
-        "n_jobs": 100
+        "proxy_timeout": 30
     }
 
     collector_kwargs = {
@@ -28,5 +27,13 @@ if __name__ == "__main__":
     }
 
     proxy_collector = ProxyCollector(**collector_kwargs)
-    valid_proxies = proxy_collector.collect_proxies()
-    print(valid_proxies)
+    valid_proxies = await proxy_collector.collect_proxies()
+    return valid_proxies
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop_result = loop.run_until_complete(asyncio.gather(main()))
+    loop.close()
+
+    print(loop_result)
