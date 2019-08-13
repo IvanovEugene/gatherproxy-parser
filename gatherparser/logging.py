675afc90c78c0e7ffbb11f51f2827648714f75c9
Ivanov Eugene <erqups@gmail.com>
2019-08-13 11:13:41 +0300

    feat: add main logger

--- gatherparser/logging.py
+++ gatherparser/logging.py
@@ -1,10 +1,17 @@
+# encoding: utf-8
+# TODO: migrate to aiologger
 import sys
 import logging
 
 
-def get_loggger(module_name: str, filename: str = "logfile.log") -> logging.Logger:
-    logger = logging.getLogger(module_name)
-    logger.setLevel(logging.DEBUG)
+def get_logger(module_name: str) -> logging.Logger:
+    # Main (module) logger
+    main_logger = logging.getLogger("gatherparser")
+    main_logger.setLevel(logging.INFO)
+    # Concrete logger
+    logger = main_logger.getChild(module_name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
     # Stdout handler
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.addFilter(lambda entry: entry.levelno <= logging.INFO)
@@ -15,10 +22,5 @@ def get_loggger(module_name: str, filename: str = "logfile.log") -> logging.Logg
     stderr_handler.addFilter(lambda entry: entry.levelno > logging.INFO)
     stderr_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(module)s [%(name)s] %(message)s'))
     logger.addHandler(stderr_handler)
-    # File handler
-    file_handler = logging.FileHandler(filename=filename, encoding="utf8")
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(module)s [%(name)s] %(message)s'))
-    logger.addHandler(file_handler)
 
     return logger
