dd5f3092b1a883aa82ba1a1334233f9f39d1b8f6
Ivanov Eugene <erqups@gmail.com>
2019-08-13 16:34:58 +0300

    feat: remove PhantomJSDriver class

--- gatherparser/utils/drivers.py
+++ gatherparser/utils/drivers.py
@@ -1,4 +1,4 @@
-# encoding: utf-8 #
+# encoding: utf-8
 from abc import ABC, abstractmethod
 from selenium import webdriver
 
@@ -24,19 +24,6 @@ class ChromeDriver(AbstractDriver):
 
         driver = webdriver.Chrome(executable_path=self.driver_path,
                                   chrome_options=chrome_options)
-        if self.page_load_timeout:
-            driver.set_page_load_timeout(self.page_load_timeout)
-
-        return driver
-
-
-class PhantomJSDriver(AbstractDriver):
-    def __init__(self, driver_path, page_load_timeout=None):
-        self.page_load_timeout = page_load_timeout
-        self.driver_path = driver_path
-
-    def get_driver(self):
-        driver = webdriver.PhantomJS(executable_path=self.driver_path)
         if self.page_load_timeout:
             driver.set_page_load_timeout(self.page_load_timeout)
 
