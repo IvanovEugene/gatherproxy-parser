65064b32de3d033ce89ce6d64f439bb051094858
Ivanov Eugene <erqups@gmail.com>
2019-08-26 14:39:26 +0300

    feat: remove level settings when creating a logger

--- gatherparser/utils/logging.py
+++ gatherparser/utils/logging.py
@@ -5,9 +5,8 @@ import logging
 
 class Logging:
     @staticmethod
-    def get_logger(name: str, level=logging.DEBUG) -> logging.Logger:
+    def get_logger(name: str) -> logging.Logger:
         logger = logging.getLogger(name)
-        logger.setLevel(level)
         logger.propagate = True
 
         return logger
